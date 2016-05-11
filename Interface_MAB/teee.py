632f44f5d6dad1509af49399471b4ad20a66f5e5
joker <joker@debian>
2016-05-11 17:04:42 +0300

    work

--- Interface_MAB/teee.py
+++ Interface_MAB/teee.py
@@ -8,9 +8,12 @@ except:
       pass
 try:
     import gtk
+    import gtk.gdk
     import gtk.glade
     import hal
     import gladevcp.makepins
+    import pango
+    import string
     from gladevcp.gladebuilder import GladeBuilder
 except:
     sys.exit(1)
@@ -23,7 +26,22 @@ class HellowWorldGTK:
 #        halcomp = hal.component("work")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("MainWindow")
+#+ Устанавливаем шрифты и все что не смог гладе
+        lABELFont = pango.FontDescription("Tahoma 24")
+        self.builder.get_object("ProgName").modify_font(lABELFont)
+        self.builder.get_object("label4").modify_font(lABELFont)
+        self.builder.get_object("label5").modify_font(lABELFont)
+        self.builder.get_object("label6").modify_font(lABELFont)
+        self.builder.get_object("label7").modify_font(lABELFont)
+        self.builder.get_object("label8").modify_font(lABELFont)
+        self.builder.get_object("label9").modify_font(lABELFont)
+        self.builder.get_object("label15").modify_font(lABELFont)
+#- Устанавливаем шрифты и все что не смог гладе       
         self.window.show()
+        self.Click_Button_panel(self.builder.get_object('f1'))
+
+        
+        
 #        panel = gladevcp.makepins.GladePanel( halcomp, "work.glade", self.builder, None)
  #       halcomp.ready()
         
@@ -34,7 +52,48 @@ class HellowWorldGTK:
 #         halcomp = hal.component("face1")
 #         self.glade.connect_signals(self)
 #         self.glade.get_object("MainWindow").show_all()
+    def Set_Font_Text_Button(self,widget,text):
+        obj=self.builder.get_object(widget)
+        obj.set_label(text)
+        for widget2 in obj.get_children(): 
+            if (isinstance(widget2, gtk.Label)):
+                widget2.set_justify(gtk.JUSTIFY_CENTER)
 
+    def Click_Button_panel(self, widget):
+        #+ Подкаждую закладку меняем кнопки
+
+        
+        NameButton = widget.get_label()
+        if (NameButton.translate(None, string.whitespace).find('F1Начатьвыделение')==0):
+            self.textbuffer = self.builder.get_object("textview3").get_buffer()
+            self.Tag = self.textbuffer.create_tag("Selected",background='yellow',size_points=24.0)
+#            tttt=gtk.TextView
+#            tttt.get_buffer().s
+            print self.Tag
+            
+        
+        elif (NameButton.translate(None, string.whitespace).find('F5Редакторпрограмм')==0):
+            self.Set_Font_Text_Button('f1','F1 \nНачать\nвыделение')
+            self.Set_Font_Text_Button('f2','F2 \nЗакончить\nвыделение')
+            self.Set_Font_Text_Button('f3','F3 \nСкопировать\n ')
+            self.Set_Font_Text_Button('f4','F4 \nВырезать\n ')
+            self.Set_Font_Text_Button('f5','F5 \nВставить\n ')
+            self.Set_Font_Text_Button('f6','F6 \n  \n ')
+            self.Set_Font_Text_Button('f7','F7 \n  \n ')
+            self.Set_Font_Text_Button('f8','F8 \nВернуться\n на \nГлавную')
+            self.builder.get_object("notebook1").set_current_page(4)
+        else:
+            self.Set_Font_Text_Button('f1','F1 \nРучное \nУправление')
+            self.Set_Font_Text_Button('f2','F2 \nAUTO\n')
+            self.Set_Font_Text_Button('f3','F3 \nНоль Детали\n')
+            self.Set_Font_Text_Button('f4','F4 \nКоррекция\nинструмента')
+            self.Set_Font_Text_Button('f5','F5 \nРедактор\nпрограмм')
+            self.Set_Font_Text_Button('f6','F6 \nГрафика\n')
+            self.Set_Font_Text_Button('f7','F7 \nСервис\n')
+            self.Set_Font_Text_Button('f8','F8 \nСистема\n')
+            self.builder.get_object("notebook1").set_current_page(0)
+
+    
     def on_MainWindow_delete_event(self, widget, event):
         gtk.main_quit()
         
--- Interface_MAB/test.py
+++ Interface_MAB/test.py
@@ -1,5 +1,41 @@
-'''
-Created on 10 мая 2016 г.
+#!/usr/bin/env python
 
-@author: joker
-'''
+# import libraries
+import gtk
+import sys,os
+import linuxcnc, hal
+import gladevcp.makepins
+
+# set up paths to files
+BASE = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
+libdir = os.path.join(BASE, "lib", "python")
+sys.path.insert(0, libdir)
+datadir = os.path.join(BASE, "share", "linuxcnc")
+xmlname = os.path.join(datadir,"gui6.glade")
+
+# main class
+class gui6(object):
+  def __init__(self):
+    self.builder = gtk.Builder()
+    self.builder.add_from_file(xmlname)
+    self.builder.connect_signals(self)
+    self.window = self.builder.get_object("window1")
+    self.halcomp = hal.component("gui6")
+    self.panel = gladevcp.makepins.GladePanel(self.halcomp, xmlname, self.builder, None)
+    self.halcomp.ready()
+    self.window.show()
+
+  # create methods to handle the signal handlers
+
+  def on_window1_destroy(self, widget, data=None):
+    print "quit with cancel"
+    gtk.main_quit()
+
+  def on_menu_quit_activate(self, menuitem, data=None):
+    print "quit from menu"
+    gtk.main_quit()
+
+# run the program
+if __name__ == "__main__":
+  app = gui6() # initialize an instance of the gui6 class called app
+  gtk.main() # start monitoring signals from our program
