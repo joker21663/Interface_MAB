787183f8339c87a61b29e3b214861df4464a94b6
joker <joker@localhost.localdomain>
2016-05-27 12:52:02 +0300

    Signed-off-by: joker <joker@localhost.localdomain>

--- Interface_MAB/teee.py
+++ Interface_MAB/teee.py
@@ -18,6 +18,9 @@ try:
     import string
     import sys,os
     import math
+    import pprint
+    from gscreen import mdi
+    from gscreen import emc_interface
     from gladevcp.gladebuilder import GladeBuilder
 except:
     sys.exit(1)
@@ -32,7 +35,6 @@ class HellowWorldGTK:
     def __init__(self):
 #  Константы всякие
         self.type_gremlin_view = ("x","y","z","p")
-
 #  Установка emc       
         self.emc = linuxcnc
         self.status = self.emc.stat()
@@ -42,6 +44,15 @@ class HellowWorldGTK:
         self.command.state(linuxcnc.STATE_ESTOP_RESET)
         self.command.state(linuxcnc.STATE_ON)
         self.jog_distance=0
+        self.curr_coordinate='G54' # П54 и прочее
+        mdi_labels = mdi_eventboxes = []
+        self.mdi_control = mdi.mdi_control(gtk, linuxcnc, mdi_labels, mdi_eventboxes)
+#        self.command.home(0)
+#        self.command.home(1)
+#        self.command.home(2)
+        self.emcgscreen=emc_interface.emc_control(linuxcnc)
+        self.emcgscreen.home_all(1)
+
 #        inifile = linuxcnc.ini(sys.argv[2])
 #        print inifile
 #        for (gcod) in self.status.gcodes():
@@ -51,7 +62,6 @@ class HellowWorldGTK:
         datadir = os.path.join(BASE, "usr", "share", "linuxcnc")
         xmlname = os.path.join(datadir,"work.glade")
         self.builder.add_from_file(xmlname)
-#        halcomp = hal.component("work")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("MainWindow")
 #  Установка HAL         
@@ -60,17 +70,18 @@ class HellowWorldGTK:
         self.machine_status = 0
 #+ Устанавливаем шрифты и все что не смог гладе
         lABELFont = pango.FontDescription("Tahoma 20")
-        self.builder.get_object("ProgName").modify_font(lABELFont)
-        self.builder.get_object("label4").modify_font(lABELFont)
-        self.builder.get_object("label5").modify_font(lABELFont)
-        self.builder.get_object("label6").modify_font(lABELFont)
-        self.builder.get_object("label7").modify_font(lABELFont)
-        self.builder.get_object("label8").modify_font(lABELFont)
-        self.builder.get_object("label9").modify_font(lABELFont)
-        self.builder.get_object("label15").modify_font(lABELFont)
+#        self.builder.get_object("ProgName").modify_font(lABELFont)
+#        self.builder.get_object("label4").modify_font(lABELFont)
+#        self.builder.get_object("label5").modify_font(lABELFont)
+#        self.builder.get_object("label6").modify_font(lABELFont)
+#        self.builder.get_object("label7").modify_font(lABELFont)
+#        self.builder.get_object("label8").modify_font(lABELFont)
+#        self.builder.get_object("label9").modify_font(lABELFont)
+#        self.builder.get_object("label15").modify_font(lABELFont)
 #        self.builder.get_object("label20").modify_font(lABELFont)
         self.builder.get_object("ProgName1").modify_font(lABELFont)
         self.builder.get_object("label27").modify_font(lABELFont)
+        self.builder.get_object("label40").modify_font(lABELFont)
         self.builder.get_object("label24").modify_font(lABELFont)
         self.builder.get_object("label25").modify_font(lABELFont)
         self.builder.get_object("label26").modify_font(lABELFont)
@@ -86,7 +97,33 @@ class HellowWorldGTK:
         self.Set_Big_Fat(self.builder.get_object("label35"))
         self.Set_Big_Fat(self.builder.get_object("label36"))
         self.Set_Big_Fat(self.builder.get_object("label37"))
+        self.Set_Big_Fat(self.builder.get_object("label4"))
+        self.Set_Big_Fat(self.builder.get_object("label5"))
+        self.Set_Big_Fat(self.builder.get_object("label6"))
+        self.Set_Big_Fat(self.builder.get_object("label7"))
+        self.Set_Big_Fat(self.builder.get_object("label8"))
+        self.Set_Big_Fat(self.builder.get_object("label9"))
+        
+        font = pango.FontDescription('Tahoma 40')
+        self.builder.get_object("entry2").set_text('0')
+        self.builder.get_object("entry2").modify_font(font)
+        self.builder.get_object("entry3").set_text('0')
+        self.builder.get_object("entry3").modify_font(font)
+        self.builder.get_object("entry4").set_text('0')
+        self.builder.get_object("entry4").modify_font(font)
 
+
+#        self.builder.get_object("textview1").modify_font(font)
+#        self.builder.get_object("textview2").modify_font(font)
+#        self.builder.get_object("textview7").modify_font(font)
+
+
+
+
+#        self.Set_Big_Fat(self.builder.get_object("textview1"))
+#        self.Set_Big_Fat(self.builder.get_object("textview2"))
+#        self.Set_Big_Fat(self.builder.get_object("textview7"))
+        
 
         self.builder.get_object("label32").modify_font(lABELFont)
         self.builder.get_object("label33").modify_font(lABELFont)
@@ -118,27 +155,75 @@ class HellowWorldGTK:
         while i<10:
            self.builder.get_object('f'+str(i)).set_style(self.default_style_button)
            i=i+1 
-
-
         self.window.show()
-#        self.builder.get_object('hidenButton').hide()
         self.builder.get_object('hidenButton').show()
         self.Click_Button_panel(self.builder.get_object('f9'))
+# Зеленые кнопочги
+# Установка цветных кнопок
 #+ Тэг на выделение
         self.Tag = self.builder.get_object("textview3").get_buffer().create_tag("Selected",background='yellow',size_points=24.0)
 #- Тэг на выделение 
         self.timelock()      
-        
-#        panel = gladevcp.makepins.GladePanel( halcomp, "work.glade", self.builder, None)
- #       halcomp.ready()
-        
-#         
-#         self.gladefile = "face1.glade" 
-#         self.glade = gtk.Builder()
-#         self.glade.add_from_file(self.gladefile)
-#         halcomp = hal.component("face1")
-#         self.glade.connect_signals(self)
-#         self.glade.get_object("MainWindow").show_all()
+        inifile = linuxcnc.ini(sys.argv[2])
+        toolfile = inifile.find('EMCIO', 'TOOL_TABLE')
+        cycle_time = inifile.find('EMCIO', 'CYCLE_TIME')
+        dir_ini=os.path.dirname(sys.argv[2])
+        self.tool_file = dir_ini+"/"+toolfile
+# редактор инструмента загрузим из файла и занесем в табличку
+        self.liststore = self.builder.get_object('liststore1')
+        self.treeview1=self.builder.get_object('treeview1')
+        self.builder.get_object("cellrenderertext2").connect("edited", self.editToolTable, self.liststore,1)
+        self.builder.get_object("cellrenderertext3").connect("edited", self.editToolTable, self.liststore,2)
+
+        with open(self.tool_file,'r') as fin:
+            linesfile = fin.read().split("\n")
+            linesfile = filter(lambda x:len(x.strip(' '))>0,linesfile )
+            for x in linesfile:
+                ToolLine=x.split(' ')
+                ToolLine=filter(lambda x:len(x.strip(' '))>0,ToolLine)
+                Tnumber =filter(lambda x:x.find('T')==0,ToolLine)
+                Znumber =filter(lambda x:x.find('Z')==0,ToolLine)
+                Dnumber =filter(lambda x:x.find('D')==0,ToolLine)
+                self.liststore.append([int(Tnumber[0].replace('T','')), float(Znumber[0].replace('Z','')), float(Dnumber[0].replace('D',''))])
+# Ввод координат окончание
+    def end_coordinate(self,widget,new_text):
+        if (widget.get_editable()==False):
+            return
+        X_coor=float(self.builder.get_object("label17").get_text())
+        Y_coor=float(self.builder.get_object("label18").get_text())
+        Z_coor=float(self.builder.get_object("label39").get_text())
+        Cur_coord=0
+        axis_number=0
+        if (widget==self.builder.get_object("entry2")):
+            Cur_coord=X_coor
+            axis_number=0
+        elif (widget==self.builder.get_object("entry3")):
+            Cur_coord=Y_coor
+            axis_number=1   
+        elif (widget==self.builder.get_object("entry4")):
+            Cur_coord=Z_coor
+            axis_number=2
+        widget.set_text(str(Cur_coord+float(widget.get_text())))
+    
+        self.SetAxis(axis_number,float(widget.get_text()))
+#        print(gtk.Buildable.get_name(widget))
+
+
+# Редактор инструмента
+    def save_reload_tool_table(self):
+        with open(self.tool_file,'w') as output:
+            for el in self.liststore:
+                Tool_STR = 'T'+str(el[0])+" P"+str(el[0])+" Z"+str(el[1])+" D"+str(el[2])+'\n' 
+                output.write(Tool_STR)
+        self.command.load_tool_table()        
+
+
+    def editToolTable(self, widget, row, new_text, model, column):
+          iter = self.liststore.get_iter(row)
+          self.liststore.set_value(iter, column, float(new_text))
+          if len(self.liststore)>0:
+            self.save_reload_tool_table()
+
     def timelock(self):
         gobject.timeout_add(100, self.periodic) 
 
@@ -174,9 +259,19 @@ class HellowWorldGTK:
         tgg=os.path.split(self.status.file)
         self.builder.get_object("ProgName1").set_label(tgg[1])
         pos=self.status.actual_position
-        self.builder.get_object("label35").set_text('%.3f'%(pos[0]))
-        self.builder.get_object("label36").set_text('%.3f'%(pos[1]))
-        self.builder.get_object("label37").set_text('%.3f'%(pos[2]))
+        posOff = self.status.g5x_offset
+        self.builder.get_object("label35").set_text('%.3f'%(posOff[0]))
+        self.builder.get_object("label36").set_text('%.3f'%(posOff[1]))
+        self.builder.get_object("label37").set_text('%.3f'%(posOff[2]))
+        
+        self.builder.get_object("label17").set_text('%.3f'%(pos[0]))
+        self.builder.get_object("label18").set_text('%.3f'%(pos[1]))
+        self.builder.get_object("label39").set_text('%.3f'%(pos[2]))
+
+        self.Set_Big_Fat(self.builder.get_object("label17"))
+        self.Set_Big_Fat(self.builder.get_object("label18"))
+        self.Set_Big_Fat(self.builder.get_object("label39"))
+
         self.Set_Big_Fat(self.builder.get_object("label35"))
         self.Set_Big_Fat(self.builder.get_object("label36"))
         self.Set_Big_Fat(self.builder.get_object("label37"))
@@ -234,8 +329,16 @@ class HellowWorldGTK:
         if (self.jog_distance==0.1):
             self.builder.get_object("f5").set_style(self.gren_style_button)
             self.builder.get_object('hidenButton').show()
-         
-
+    def set_color_button_G_5x(self,widget):
+            self.builder.get_object("f1").set_style(self.default_style_button)
+            self.builder.get_object("f2").set_style(self.default_style_button)
+            self.builder.get_object("f3").set_style(self.default_style_button)
+            self.builder.get_object("f4").set_style(self.default_style_button)
+            self.builder.get_object("f5").set_style(self.default_style_button)
+            self.builder.get_object("f6").set_style(self.default_style_button)
+            print self.builder.get_object("notebook1").get_current_page()
+            if (self.builder.get_object("notebook1").get_current_page()==3):
+                widget.set_style(self.gren_style_button)
     def Jog_Operation(self, widget):
         NameButton = widget.get_label()
         NameButton =NameButton.translate(None, string.whitespace)
@@ -267,6 +370,12 @@ class HellowWorldGTK:
             buff=self.builder.get_object("textview6").get_buffer()
             txtB=buff.get_text(buff.get_start_iter(),buff.get_end_iter())
             buff.set_text(txtB[0:len(txtB)-1])
+    def SetAxis(self,axis,value):
+            d = {0: 'x', 1: 'y', 2:'z'}
+            m = "G10 L20 P0 %s%f"%(d[axis],value)
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi(m)
+
     def Click_Button_panel(self, widget):
         #+ Подкаждую закладку меняем кнопки
         global first_position
@@ -367,7 +476,12 @@ class HellowWorldGTK:
         elif (NameButton.translate(None, string.whitespace).find('F8Выход')==0):
             gtk.main_quit()
         elif (NameButton.translate(None, string.whitespace).find('F7Test')==0):
-            self.command.home(0)
+#            self.command.home(0)
+#            self.command.wait_complete(5)
+#            self.command.home(1)
+#            self.command.wait_complete(5)
+#            self.command.home(2)
+#            self.command.wait_complete(5)
             print self.machine_status
         elif (NameButton.translate(None, string.whitespace).find('F1СменитьВид')==0):
             index = self.type_gremlin_view.index(self.gremlin.get_property('view'))+1
@@ -376,8 +490,14 @@ class HellowWorldGTK:
             self.gremlin.set_property('view',self.type_gremlin_view[index])
         elif (NameButton.translate(None, string.whitespace).find('F2ВключитьВыключитьDRO')==0):
             if (self.gremlin.get_property('enable_dro')==0):
+                self.gremlin.set_property('use_commanded',(1))
+                self.gremlin.set_property('use_relative',(1))
+                self.gremlin.set_property('show_dtg',(1))
                 self.gremlin.set_property('enable_dro',(1))
             else:
+                self.gremlin.set_property('show_dtg',(0))
+                self.gremlin.set_property('use_commanded',(0))
+                self.gremlin.set_property('use_relative',(0))
                 self.gremlin.set_property('enable_dro',(0))
         elif (NameButton.translate(None, string.whitespace).find('F3Приблизить')==0):
             self.gremlin.zoom_in()
@@ -452,6 +572,38 @@ class HellowWorldGTK:
             self.Set_Font_Text_Button('button3','F3 \nУдалить символ\n')            
             self.MDIWindows.show()
             self.builder.get_object("textview6").grab_focus();
+        elif (NameButton.translate(None, string.whitespace).find('F1G54')==0):
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G54'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G54')
+        elif (NameButton.translate(None, string.whitespace).find('F2G55')==0):
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G55'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G55')
+        elif (NameButton.translate(None, string.whitespace).find('F3G56')==0):
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G56'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G56')
+        elif (NameButton.translate(None, string.whitespace).find('F4G57')==0):
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G57'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G57')
+        elif (NameButton.translate(None, string.whitespace).find('F5G58')==0):    
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G58'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G58')      
+        elif (NameButton.translate(None, string.whitespace).find('F6G59')==0):     
+            self.set_color_button_G_5x(widget)
+            self.curr_coordinate='G59'
+            self.command.mode(linuxcnc.MODE_MDI)
+            self.command.mdi('G59')
+        elif (NameButton.translate(None, string.whitespace).find('F7РедакторТаблицы')==0):     
+            print 'g' 
 
         elif (NameButton.translate(None, string.whitespace).find('F4Допклавиши')==0): 
             self.Set_Font_Text_Button('f1','F1 \nВыбор файла\nпрограммы')
@@ -522,9 +674,34 @@ class HellowWorldGTK:
             self.Set_Font_Text_Button('f8','F8 \nВращение\nшпинделя\nвлево')
             self.Set_Font_Text_Button('f9','\nГлавное\nменю')
             self.builder.get_object("notebook1").set_current_page(0)        
+        elif (NameButton.translate(None, string.whitespace).find('F4Коррекцияинструмента')==0):
+            self.Set_Font_Text_Button('f1','F1 \n\n')
+            self.Set_Font_Text_Button('f2','F2 \n\n')
+            self.Set_Font_Text_Button('f3','F3 \n\n ')
+            self.Set_Font_Text_Button('f4','F4 \n\n ')
+            self.Set_Font_Text_Button('f5','F5 \n\n')
+            self.Set_Font_Text_Button('f6','F6 \n\n')
+            self.Set_Font_Text_Button('f7','F7 \n\n')
+            self.Set_Font_Text_Button('f8','F8 \n\n')
+            self.Set_Font_Text_Button('f9','\n\n')
+            self.builder.get_object("notebook1").set_current_page(4)   
+        elif (NameButton.translate(None, string.whitespace).find('F3НольДетали')==0):
+            self.Set_Font_Text_Button('f1','F1 \nG54\n')
+            self.Set_Font_Text_Button('f2','F2 \nG55\n')
+            self.Set_Font_Text_Button('f3','F3 \nG56\n ')
+            self.Set_Font_Text_Button('f4','F4 \nG57\n ')
+            self.Set_Font_Text_Button('f5','F5 \nG58\n')
+            self.Set_Font_Text_Button('f6','F6 \nG59\n')
+            self.Set_Font_Text_Button('f7','F7 \nРедактор\nТаблицы')
+            self.Set_Font_Text_Button('f8','F8 \n\n')
+            self.Set_Font_Text_Button('f9','\n\n')
+            self.builder.get_object("notebook1").set_current_page(3)
+            self.builder.get_object("f1").set_style(self.gren_style_button)  
+            self.curr_coordinate='G54'
         else:
             self.jog_distance=0
             self.set_color_button()
+            self.set_color_button_G_5x(widget)
             self.Set_Font_Text_Button('f1','F1 \nРучное \nУправление')
             self.Set_Font_Text_Button('f2','F2 \nAUTO\n')
             self.Set_Font_Text_Button('f3','F3 \nНоль Детали\n')
--- Interface_MAB/test2.py
+++ Interface_MAB/test2.py
@@ -1,22 +1,34 @@
-import gtk
+from gscreen import mdi
+import pprint
 
-win = gtk.Window()
-win.connect("destroy", gtk.main_quit)
+class Greeter:
 
-btn = gtk.Button("test")
+  def __init__(self):
 
-#make a gdk.color for red
-map = btn.get_colormap() 
-color = map.alloc_color("red")
+    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    self.box = gtk.VBox()
+    self.window.add(self.box)
 
-#copy the current style and replace the background
-style = btn.get_style().copy()
-style.bg[gtk.STATE_NORMAL] = color
+    self.label = gtk.Label("Please enter your name in the box below:")
+    self.namebox = gtk.Entry(12)
+    self.button = gtk.Button("Greet Me!")
+    self.output = gtk.Label("Your output will appear here.")
 
-#set the button's style to the one you created
-btn.set_style(style)
+    self.box.pack_start(self.label, False, False, 2)
+    self.box.pack_start(self.namebox, False, False, 2)
+    self.box.pack_start(self.button, False, False, 2)
+    self.box.pack_start(self.output, False, False, 2)
 
-win.add(btn)
-win.show_all()
+    self.label.show()
+    self.namebox.show()
+    self.button.show()
+    self.output.show()
+    self.box.show()
+    self.window.show()
 
-gtk.main()
+  def main(self):
+    pprint(mdi)
+    gtk.main()
+
+    a = Greeter()
+    a.main()
